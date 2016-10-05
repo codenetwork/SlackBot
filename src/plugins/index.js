8ed10508c9e1b6c3d95cae001fe1498901147163
Byron Mejia <byron.mejia@connect.qut.edu.au>
2016-10-05 20:43:55 +1000

    Changing it live

--- src/plugins/index.js
+++ src/plugins/index.js
@@ -1,9 +1,12 @@
 import clientAuth from './clientAuthenticatedPlugin';
 import helloWorld from './helloWorld';
+import antarcticWeather from './antarcticWeather';
+
 
 function autoLoadPlugins(opts) {
   clientAuth(opts);
   helloWorld(opts);
+  antarcticWeather(opts);
 }
 
 export default opts => autoLoadPlugins(opts);
